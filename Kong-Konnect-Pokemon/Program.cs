1ed40153cbe30a83a4c6056698518f903725e53e
jeff06 <21128028+jeff06@users.noreply.github.com>
2026-01-25 16:46:10 -0500

    feat: merging kong and pokemon services

--- Kong-Konnect-Pokemon/Program.cs
+++ Kong-Konnect-Pokemon/Program.cs
@@ -1,3 +1,5 @@
+using Kong_Konnect_Pokemon;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,11 +14,13 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
 
-app.MapGet("/api/v2/pokemon/Pikachu", () => "Pikachu from Local");
-app.MapGet("/api/v2/pokemon/Charizard", () => "Charizard from Local");
-app.MapGet("/api/v2/pokemon/Eevee", () => "Eevee from Local");
+app.MapGet("/api/v2/pokemon/Pikachu", () => Results.Ok(new SimpleClass("Pikachu from Local")));
+app.MapGet("/api/v2/pokemon/Charizard", () => Results.Ok(new SimpleClass("Charizard from Local")));
+app.MapGet("/api/v2/pokemon/Eevee", () => Results.Ok(new SimpleClass("Eevee from Local")));
+app.MapGet("/status/healthy", () => Results.Ok(new SimpleClass("Healthy")));
+app.MapGet("/status/unhealthy", () => Results.InternalServerError(new SimpleClass("Unhealthy")));
 
 app.Run();
---
+++ Kong-Konnect-Pokemon/SimpleClass.cs
@@ -0,0 +1,6 @@
+namespace Kong_Konnect_Pokemon;
+
+public class SimpleClass(string value)
+{
+    public string Value { get; set; } = value;
+}
